636671437ea2e97098a822f79d9628fc57152f0b
弦間由樹 <gemma.naok@gmail.com>
2024-10-22 00:03:05 +0900

    feature: add random util

--- backend/main_test.go
+++ backend/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"backend/util"
 	"bytes"
 	"encoding/json"
 	"net/http"
@@ -20,36 +21,40 @@ func setupTestDB(t *testing.T) *gorm.DB {
 	err = db.AutoMigrate(&User{})
 	err = db.AutoMigrate(&Post{})
 	require.NoError(t, err)
+	db.Exec("TRUNCATE TABLE users")
+	db.Exec("TRUNCATE TABLE posts")
 
 	return db
 }
 
-func CreateTestUsers(t *testing.T, db *gorm.DB) {
-	bio := "bio example"
-	bio2 := "bio example2"
-	users := []User{
-		{
-			Name:     "Gem",
-			Email:    "example1@mail.com",
-			Password: "test",
-			Bio:      &bio,
-		},
-		{
-			Name:     "Mma",
-			Email:    "example8@mail.com",
-			Password: "test2",
-			Bio:      &bio2,
-		},
+func CreateTestUser(t *testing.T, db *gorm.DB) (*User, error) {
+	user := User{
+		Name:     util.RandomString(5),
+		Email:    util.RandomEmail(5),
+		Password: util.RandomString(10),
+		Bio:      util.RondomBio(20),
 	}
-	for _, user := range users {
-		err := db.Create(&user).Error
-		require.NoError(t, err)
+	err := db.Create(&user).Error
+	if err != nil {
+		return nil, err
 	}
+	require.NoError(t, err)
+	return &user, nil
+}
+
+func CreateTestUsers(t *testing.T, count int, db *gorm.DB) []User {
+	var users []User
+	for i := 0; i < count; i++ {
+		user, _ := CreateTestUser(t, db)
+		users = append(users, *user)
+	}
+	return users
 }
 
 func TestGetUsersHandler(t *testing.T) {
 	db := setupTestDB(t)
-	CreateTestUsers(t, db)
+
+	testUsers := CreateTestUsers(t, 2, db)
 
 	req, err := http.NewRequest("GET", "/users", nil)
 	require.NoError(t, err)
@@ -64,14 +69,40 @@ func TestGetUsersHandler(t *testing.T) {
 	var users []User
 	err = json.Unmarshal(rec.Body.Bytes(), &users)
 	require.NoError(t, err)
-	require.Len(t, users, 2)
-	require.Equal(t, "Gem", users[0].Name)
-	require.Equal(t, "Mma", users[1].Name)
+	require.Equal(t, testUsers[0].Name, users[0].Name)
+	require.Equal(t, testUsers[1].Email, users[1].Email)
+	require.Equal(t, testUsers[1].CreatedAt, users[1].CreatedAt)
 }
+
+func TestCreateUserHandler(t *testing.T) {
+	user := User{
+		Name:     util.RandomString(5),
+		Email:    util.RandomEmail(8),
+		Password: util.RandomString(10),
+		Bio:      util.RondomBio(20),
+	}
+	reqBody, err := json.Marshal(&user)
+
+	req, err := http.NewRequest("POST", "/users", bytes.NewBuffer(reqBody))
+	require.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+
+	db := setupTestDB(t)
+	handler := CreateUserHandler(db)
+	handler.ServeHTTP(rec, req)
+	require.Equal(t, http.StatusOK, rec.Code)
+
+	var createdUser User
+	err = json.NewDecoder(rec.Body).Decode(&createdUser)
+	require.NoError(t, err)
+	require.Equal(t, user.Name, createdUser.Name)
+}
+
 func TestCreatePostHandler(t *testing.T) {
 	testPost := Post{
 		UserID:  1,
-		Content: "test content",
+		Content: util.RandomString(30),
 	}
 	reqBody, _ := json.Marshal(testPost)
 	req, err := http.NewRequest("POST", "/posts", bytes.NewBuffer(reqBody))
